97afe1359c19b9f0a170ec0f10f7137fd832fb61
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 08:31:48 -0300

    feat: delete user

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -28,11 +28,25 @@ app.get('/users/:id', async (req, res) => {
   const content = await fs.readFile(contentPath, 'utf8');
   const users = JSON.parse(content);
   const foundUser = users.find((user) => user.id === id);
-  if (foundUser === undefined) {
+  if (!foundUser) {
     return res.status(STATUS_NOT_FOUND).json({ message: 'User Not Found'});
   }
   const { password: _, ...rest } = foundUser;
   return res.status(STATUS_CODE_OK).json(rest)
 })
+
+app.delete('/users/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const contentPath = path.resolve(__dirname, '..', '..', 'data', 'users.json');
+  const content = await fs.readFile(contentPath, 'utf8');
+  const users = JSON.parse(content);
+  const foundUser = users.find((user) => user.id === id);
+  if (!foundUser) {
+    return res.status(STATUS_NOT_FOUND).json({ message: 'User Not Found'});
+  }
+  const filterUsers = users.filter((user) => user.id !== id);
+  await fs.writeFile(contentPath, JSON.stringify(filterUsers, null, 2), 'utf8');
+  return res.sendStatus(STATUS_CODE_NO_CONTENT);
+});
 
 module.exports = app
