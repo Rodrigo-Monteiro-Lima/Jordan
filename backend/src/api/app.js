362f8d21f7d4eae7b0a7a6196f5883ec7c87511d
Rodrigo <rodrigoml2395@gmail.com>
2023-02-18 08:46:17 -0300

    feat: add user

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -49,4 +49,16 @@ app.delete('/users/:id', async (req, res) => {
   return res.sendStatus(STATUS_CODE_NO_CONTENT);
 });
 
+app.post('/users', async (req, res) => {
+  const { body } = req;
+  const contentPath = path.resolve(__dirname, '..', '..', 'data', 'users.json');
+  const content = await fs.readFile(contentPath, 'utf8');
+  const users = JSON.parse(content);
+  const newId = users.map(({id}) => id).sort()[users.length - 1] + 2;
+  const newUser = {id: newId, ...body }
+  users.push(newUser);
+  await fs.writeFile(contentPath, JSON.stringify(users, null, 2), 'utf8');
+  return res.status(STATUS_CODE_CREATED).json(newUser);
+});
+
 module.exports = app
