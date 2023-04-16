527cf578a288abeaf6d95a1793c1488b4dc15803
Rodrigo <rodrigoml2395@gmail.com>
2023-04-15 23:03:08 -0300

    feat: get route

--- backend/src/routes/players.js
+++ backend/src/routes/players.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const {playerController} = require('../controllers/');
+const { playerController } = require('../controllers/');
 
 const playerRouter = express.Router();
 
 playerRouter.post('/', playerController.createPlayer);
+
+playerRouter.get('/', playerController.getPlayers);
+
 
 module.exports = playerRouter;
