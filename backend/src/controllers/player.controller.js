abd54ca8d2c5145c0fbf3a34492e9210c5213045
Rodrigo <rodrigoml2395@gmail.com>
2023-04-15 23:02:15 -0300

    feat: get all player

--- backend/src/controllers/player.controller.js
+++ backend/src/controllers/player.controller.js
@@ -18,30 +18,37 @@ const createPlayer = (request, response) => {
             }
           }
   } */
-   /* #swagger.responses[201] = {
-            description: 'Player created successfully.',
-            schema: {
-              "name": "Michael Jordan",
-              "birthdate": "20-06-1967",
-              "weight": 100,
-              "height": 205,
-              "local": {
-                "id": 1,
-                "name": "São Paulo"
-              },
-              "positions": [1],
-              "predominantHand": "Esquerda",
-              "currentTeam": {
-                "id": 5,
-                "dateSign": "04-01-2023"
-              }
-            }
-    } */
+  /* #swagger.responses[201] = {
+           description: 'Player created successfully.',
+           schema: {
+             "name": "Michael Jordan",
+             "birthdate": "20-06-1967",
+             "weight": 100,
+             "height": 205,
+             "local": {
+               "id": 1,
+               "name": "São Paulo"
+             },
+             "positions": [1],
+             "predominantHand": "Esquerda",
+             "currentTeam": {
+               "id": 5,
+               "dateSign": "04-01-2023"
+             }
+           }
+   } */
   const player = request.body;
   const newPlayer = playerService.createPlayer(player);
   response.status(201).json(newPlayer);
 };
 
-module.exports = { 
+const getPlayers = async (_req, res) => {
+  const players = await playerService.getPlayers();
+  console.log(players);
+  return res.status(200).json(players);
+};
+
+module.exports = {
   createPlayer,
+  getPlayers,
 };
